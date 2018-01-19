056dafa65a016fdaf22a8dc821df447d4135e1ec
liamsgotgenes <liamnowaczyk@aol.com>
2018-01-19 08:37:30 -0600

    added node comparator

---
+++ src/NodeCompare.java
@@ -0,0 +1,19 @@
+import java.util.Comparator;
+
+public class NodeCompare implements Comparator
+{
+    @Override
+    public int compare(Object a, Object b)
+    {
+        int x=((node)a).getDist();
+        int y=((node)b).getDist();
+        
+        if (x==-1&&y==-1) return 0;
+        if (x==-1) return 1;
+        if (y==-1) return -1;
+
+        if (x<y) return -1;
+        if (y<x) return 1;
+        return 0;
+    }
+}
